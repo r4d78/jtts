0ef85807d614cf666101f0d21fe546e6e75036d3
mkobayashi <mkobayashi@NPC-79.isr-corp.local>
2015-12-24 19:14:36 +0900

    カテゴリマスタ修正

--- src/main/java/bean/MainBean.java
+++ src/main/java/bean/MainBean.java
@@ -10,6 +10,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import lombok.Data;
@@ -24,6 +26,13 @@ public class MainBean {
 
     private boolean isStreaming;
     private String streamURL;
+    private String greet;
+    private boolean tab1;
+    private boolean tab2;
+    private boolean tab3;
+    private String class1;
+    private String class2;
+    private String class3;
 
     /**
      * コンストラクタ
@@ -31,20 +40,33 @@ public class MainBean {
     public MainBean() {
     }
 
-    public void checkTube() {
+    public void refreshStat() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        switch(calendar.get(Calendar.HOUR_OF_DAY)){
+            case 5: case 6: case 7: case 8: case 9: case 10:
+                greet = "はいおはようございます";
+                break;
+            case 11: case 12: case 13: case 14: case 15: case 16:
+                greet = "はいこんにちは";
+                break;
+            default:
+                greet = "はいこんばんは";
+        }
+        tab1 = true;
         try {
             URL urlObj = new URL("http://livetube.cc/index.live.json");
             HttpURLConnection http = (HttpURLConnection) urlObj.openConnection();
             http.setRequestMethod("GET");
             http.connect();
-
+            
             InputStream is = http.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
             String s;
-            String temp = null;
-            isStreaming = false;
+            streamURL = "http://h.livetube.cc/RCjinrai/";
             while ((s = reader.readLine()) != null) {
-                if(isStreaming = s.indexOf("\"link\": \"sinsin13/") > 0){
+                if(isStreaming = s.indexOf("\"link\": \"RCjinrai/") > 0){
+                    isStreaming = true;
                     streamURL = "http://h.livetube.cc/" + s.split("\"")[3];
                     break;
                 }
@@ -52,5 +74,21 @@ public class MainBean {
         } catch (Exception e) {
             isStreaming = false;
         }
+    }
+    
+    public void switch1(){
+        class1 = "active";
+        class2 = "";
+        class3 = "";
+    }
+    public void switch2(){
+        class2 = "active";
+        class1 = "";
+        class3 = "";
+    }
+    public void switch3(){
+        class3 = "active";
+        class1 = "";
+        class2 = "";
     }
 }
